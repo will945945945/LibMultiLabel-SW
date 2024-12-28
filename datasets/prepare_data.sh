752a897b7d06dd0d7ebfa929d6af009a4aef663b
yxl <d08944012@ntu.edu.tw>
2024-12-28 16:32:47 +0000

    update scripts for preparing binary datasets

--- datasets/prepare_data.sh
+++ datasets/prepare_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 root="binary_datasets"
-mkdir -p $root
-cd $root
+mkdir -p $root/raw_data
+cd $root/raw_data
 
 ## Download
 wget https://www.csie.ntu.edu.tw/\~cjlin/libsvmtools/datasets/binary/ijcnn1.bz2
@@ -21,31 +21,36 @@ for i in `ls *.bz2`; do bzip2 -d $i; done
 xz --decompress webspam_wc_normalized_unigram.svm.xz
 #xz --decompress webspam_wc_normalized_trigram.svm.xz
 echo "Completed"
+cd ../
 
 ## Dispatch, soft-link and split
 for i in a9a ijcnn1 rcv1 real-sim webspam
 do
     ds=dataset_$i
     mkdir -p $ds
-    mv $i* $ds/
     cd $ds
     if [ "$i" == "a9a" ]; then
-        ln -sf $i   trva.svm
-        ln -sf $i.t te.svm
+        ln -sf ../raw_data/$i   trva.svm
+        ln -sf ../raw_data/$i.t te.svm
+        cat trva.svm te.svm > trvate.svm
     elif [ "$i" == "ijcnn1" ]; then
-        ln -sf $i   trva.svm
-        ln -sf $i.t te.svm
+        ln -sf ../raw_data/$i   trva.svm
+        ln -sf ../raw_data/$i.t te.svm
+        cat trva.svm te.svm > trvate.svm
     elif [ "$i" == "rcv1" ]; then
-        ln -sf ${i}_train.binary trva.svm
-        ln -sf ${i}_test.binary  te.svm
+        ln -sf ../raw_data/${i}_train.binary trva.svm
+        ln -sf ../raw_data/${i}_test.binary  te.svm
+        cat trva.svm te.svm > trvate.svm
     elif [ "$i" == "real-sim" ]; then
-        ../../random_split.sh $i 90
-        ln -sf ${i}.trva trva.svm
-        ln -sf ${i}.te   te.svm
+        ../../random_split.sh ../raw_data/$i 90
+        ln -sf ../raw_data/${i}.trva trva.svm
+        ln -sf ../raw_data/${i}.te   te.svm
+        ln -sf ../raw_data/$i        trvate.svm
     else
-        ../../random_split.sh ${i}_wc_normalized_unigram.svm 90
-        ln -sf ${i}_wc_normalized_unigram.svm.trva trva.svm
-        ln -sf ${i}_wc_normalized_unigram.svm.te   te.svm
+        ../../random_split.sh ../raw_data/${i}_wc_normalized_unigram.svm 90
+        ln -sf ../raw_data/${i}_wc_normalized_unigram.svm.trva trva.svm
+        ln -sf ../raw_data/${i}_wc_normalized_unigram.svm.te   te.svm
+        ln -sf ../raw_data/${i}_wc_normalized_unigram.svm      trvate.svm
     fi
     cd ../
 done
